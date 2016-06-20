0a0df8ce55cd7c078a0420d88e27c406e6f342ed
Siva <byteshiva@users.noreply.github.com>
2016-06-20 23:26:05 +0530

    Create maybe.js

---
+++ src/maybe.js
@@ -0,0 +1,22 @@
+Maybe = function (value) {
+    var Nothing = {
+        bind: function (fn) {
+          return this;
+        },
+      };
+
+    var Something = function (value) {
+        return {
+            bind: function (fn) {
+              return Maybe(fn.call(this, value));
+            },
+          };
+      };
+
+    if (typeof value === 'undefined' || value === null)
+        return Nothing;
+
+    return Something(value);
+  };
+
+module.exports = Maybe;
